3aebbf648f2d05b58b1612a2a422d11f323694d2
Pablo Querales <119375308+PabloQuerales@users.noreply.github.com>
2025-01-18 01:13:41 +0000

    se refactorizó la lógica de los eventos onChange y OnSubmit

--- src/js/views/form.js
+++ src/js/views/form.js
@@ -3,17 +3,25 @@ import "../../styles/form.css";
 
 
 export const Form = () => {
+    const [inputValue, setInputValue] = useState({
+        name: "",
+        phone: "",
+        email: "",
+        address: ""
+    })
+
     const [hiddenMessage, setHiddenMessage] = useState("hidden")
-    async function postContact(name, phone, email, address) {
+
+    async function postContact(inputValue) {
 
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
         const raw = JSON.stringify({
-            "name": `${name}`,
-            "phone": `${phone}`,
-            "email": `${email}`,
-            "address": `${address}`
+            "name": `${inputValue.name}`,
+            "phone": `${inputValue.phone}`,
+            "email": `${inputValue.email}`,
+            "address": `${inputValue.address}`
         });
 
         const requestOptions = {
@@ -26,6 +34,12 @@ export const Form = () => {
             const response = await fetch("https://playground.4geeks.com/contact/agendas/PabloQuerales/contacts", requestOptions);
             if (response.status == 201) {
                 setHiddenMessage("")
+                setInputValue({
+                    name: "",
+                    phone: "",
+                    email: "",
+                    address: ""
+                })
             } else {
                 setHiddenMessage("hidden")
             }
@@ -36,36 +50,37 @@ export const Form = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        postContact(e.target.name.value, e.target.email.value, e.target.phone.value, e.target.address.value)
-        e.target.name.value = ""
-        e.target.email.value = ""
-        e.target.phone.value = ""
-        e.target.address.value = ""
+        postContact(inputValue)
     }
-    const [comp,setComp] = useState({
-        name:""
-        
-    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value
+        });
+    };
+
     return (
         <div className="mt-4">
             <h1 className="text-center">Add New Contact</h1>
             <h3 className={`text-center text-success ${hiddenMessage}`}>Contact Created Successfully</h3>
             <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Fullname</label>
-                    <input type="text" className="form-control" id="name" />
+                    <label for="exampleInputPassword1" className="form-label" >Fullname</label>
+                    <input type="text" className="form-control" required value={inputValue.name} name="name" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" />
+                    <input type="email" className="form-control" value={inputValue.email} name="email" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Phone</label>
-                    <input type="text" className="form-control" id="phone" />
+                    <input type="text" className="form-control" required value={inputValue.phone} name="phone" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="address" />
+                    <input type="text" className="form-control" value={inputValue.address} name="address" onChange={handleChange} />
                 </div>
                 <button type="submit" className="btn btn-primary w-100" >Save</button>
             </form>
